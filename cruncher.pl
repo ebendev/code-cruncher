57f8b7a281de029a2026644b412b0c36dc131e03
eben <eben@b3c2e11f-115f-2842-99e5-176e7098aee2>
2007-02-17 02:09:09 +0000

    strips leading and trailing whitespace from the input lines

--- cruncher.pl
+++ cruncher.pl
@@ -1,14 +1,28 @@
 #!perl -w
 use strict;
 
-print "CodeCruncher v0\n\n";
+{
+  my $errstr = "file not specified.  Correct form is:\n\$ cruncher.pl [input] [output] [optional_logfile]\n";
+  die "Input $errstr" if @ARGV < 1;
+  die "Output $errstr" if @ARGV < 2;
+}
 
-open INPUT, "< $ARGV[0]";
-open OUTPUT, "> $ARGV[1]";
-open LOG, "> $ARGV[2]";
+open INPUT, "< $ARGV[0]"
+     or die "$ARGV[0] - $!\n";
+open OUTPUT, "> $ARGV[1]"
+     or die "$ARGV[1] - $!\n";
+if($ARGV[2]) {
+  open LOG, "> $ARGV[2]";
+  select LOG;
+}
 
+print "CodeCruncher v0\n";
 
-
+while(<INPUT>) {
+  s/^\s+//;
+  s/\s+$//;
+  print OUTPUT $_;
+}
 
 close INPUT;
 close OUTPUT;
