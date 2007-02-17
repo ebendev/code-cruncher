aacf5c665088cecac595e12a18443588ced91df1
eben <eben@b3c2e11f-115f-2842-99e5-176e7098aee2>
2007-02-17 01:20:48 +0000

    using good form, opens command-line specified input, output, and log files

--- cruncher.pl
+++ cruncher.pl
@@ -1,1 +1,15 @@
-print "CodeCruncher v0\n"
+#!perl -w
+use strict;
+
+print "CodeCruncher v0\n\n";
+
+open INPUT, "< $ARGV[0]";
+open OUTPUT, "> $ARGV[1]";
+open LOG, "> $ARGV[2]";
+
+
+
+
+close INPUT;
+close OUTPUT;
+close LOG;
