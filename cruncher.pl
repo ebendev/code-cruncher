ca6716815610be7c1688282793347e27fc280cba
eben <eben@b3c2e11f-115f-2842-99e5-176e7098aee2>
2007-02-17 03:25:41 +0000

    saving non-useful code; about to majorly refactor

--- cruncher.pl
+++ cruncher.pl
@@ -16,13 +16,37 @@ if($ARGV[2]) {
   select LOG;
 }
 
-print "CodeCruncher v0\n";
+my @dependency;
+my $filestring = "";
 
+print "===> CodeCruncher Copyright 2007 Eben Geer <===============================\n";
+
+print "\n===> Original File - Start <===============================================\n";
+while(<INPUT>) { $filestring .= $_; }
+print $filestring;
+print "\n===> Original File - End <=================================================\n";
+
+print "\n===> Leading and trailing whitespace: Removing... <========================\n";
 while(<INPUT>) {
   s/^\s+//;
   s/\s+$//;
+  print $_;
+}
+print "\n===> Leading and trailing whitespace: Removed! <===========================\n";
+
+print "\n===> Comments: Removing... <===============================================\n";
+while(<INPUT>) {
+  s/<!--.*//;
+  s/.*-->//;
+  print $_;
+}
+print "\n===> Comments: Removed! <==================================================\n";
+
+print "\n===> Writing output file... <==============================================\n";
+while(<INPUT>) {
   print OUTPUT $_;
 }
+print "\n===> Output file written! <================================================\n";
 
 close INPUT;
 close OUTPUT;
