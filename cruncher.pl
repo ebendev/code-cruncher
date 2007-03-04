0fb0d59d09a9cfaf68efa22ceb91e8997490740b
eben <eben@b3c2e11f-115f-2842-99e5-176e7098aee2>
2007-03-04 00:50:42 +0000

    uncommented old code, got first part running

--- cruncher.pl
+++ cruncher.pl
@@ -3,13 +3,13 @@
 ### Pragmas ######################
 use strict;
 use FileHandle;
+use Cwd;
 
 
 ### Globals ######################
  # Path strings
-my $inputAbsPath;
-my $outputAbsPath;
-my $indexRelPath;
+my $inputRelPath;
+my $outputRelPath;
 my $testRelPath;
 
 my @names; # all identifiers in the general namespace: functions, variables, ids
@@ -107,7 +107,8 @@ my $profilePath;
 #load into @argv
 
 
-
+my $dir = getcwd;
+print $dir;
 print "\n";
 
 foreach my $el (@ARGV) {
@@ -121,7 +122,7 @@ foreach my $el (@ARGV) {
   }
   elsif($el =~ /-output:(.+)/) {
     if(defined($outputPath)) { print "$el ignored. You may only specify one output root.\n"; }
-    else { $outputPath = $1; }
+    else { $outputRelPath = $1; }
   }
   elsif($el =~ /-update:(.+)/) { $updatePaths[@updatePaths] = $1; }
   elsif($el =~ /-log:(.+)/) {
@@ -172,7 +173,7 @@ print "crunchWS = $crunchWS\n";
 print "warningsOn = $warningsOn\n";
 print "verbose = $verbose\n";
 if(defined($rootPath)) { print "rootPath = $rootPath\n"; }
-if(defined($outputPath)) { print "outputPath = $outputPath\n"; }
+if(defined($outputRelPath)) { print "outputRelPath = $outputRelPath\n"; }
 if(@updatePaths > 0) {
   print "updatePaths = ";
   print @updatePaths;
@@ -185,7 +186,7 @@ print "\n";
 
 
 # Open root
-my $rootStr;
+#my $rootStr;
 {
   $rootPath =~ s/\\/\//g;
   my $fh = new FileHandle "<$rootPath";
@@ -193,7 +194,8 @@ my $rootStr;
     print "Could not open root page: '$rootPath'\n\nExiting...\n\n";
     exit 1;
   }
-  while(<$fh>) { $rootStr .= $_; }
+  #while(<$fh>) { $rootStr .= $_; }
+  while(<$fh>) { $filestrings[0] .= $_; }
 }
 
 # Open Log
@@ -207,254 +209,191 @@ my $log;
   }
 }
 
-
-#if($rootPath =~ /([^\/]+)(?!\/)/) {
+# Isolate relative input path, filename
 if($rootPath =~ /([^\/]+)$/) {
   $filenames[0] = $1;
-  #my $temp = $filenames[0];
   if($rootPath =~ /(.*)\/$filenames[0]/) {
-  #if($rootPath =~ /(.*)($temp)/) {
-    #print "hey$1";
-    $inputAbsPath = $1;
+    $inputRelPath = $1;
   }
-  #else { print "what?"; }
 }
 
 
 printBreak("CodeCruncher Copyright 2007 Eben Geer", $verbose, $log);
-printBreak("Input Path: $inputAbsPath", $verbose, $log);
+printBreak("Input Path: $inputRelPath", $verbose, $log);
 printBreak("Original Root Page - $filenames[0] - Start", $verbose, $log);
-print $log $rootStr . "\n" if defined $log;
-print $rootStr . "\n" if $verbose;
+print $log $filestrings[0] . "\n" if defined $log;
+print $filestrings[0] . "\n" if $verbose;
 printBreak("Original Root Page - $filenames[0] - End", $verbose, $log);
 
-#{
-#  my $errstr = "file not specified.  Correct form is:\n\$ cruncher.pl [input] [output] [optional_logfile] [optional_test_module]\n";
-#  die "Input $errstr" if @ARGV < 1;
-#  die "Output $errstr" if @ARGV < 2;
-#}
 
-#open INPUT, "< $ARGV[0]"
-#     or die "$ARGV[0] - $!\n";
-#open OUTPUT, "> $ARGV[1]"
-#     or die "$ARGV[1] - $!\n";
-#if($ARGV[2]) {
-#  open LOG, "> $ARGV[2]";
-#  select LOG;
-#}
-#if($ARGV[3]) {
-##  open TESTS, "< $ARGV[3]";
-#
-##my @filenames;
-##my @filestrings;
-#
-##my $teststring;
-##my $testfile;
-#
-## do i need additional logic if ..\ prepends the individual file names?
-#
-##while($ARGV[0] =~ m"((.*\\)+)"g) {
-##  $inputpath = $1;
-##}
-#
-##while($ARGV[1] =~ m"((.*\\)+)"g) {
-##  $outputpath = $1;
-##}
-##if(defined($ARGV[3])) {
-##  if(index($inputpath, $ARGV[3]) == 0) {
-##    $testspath = substr($ARGV[3], length($inputpath));
-##    print index($inputpath, $ARGV[3]);
-##  }
-#
-#  #$ARGV[3] =~ s/($inputpath)//;
-#  #$testspath = $ARGV[3];
-##}
-#
-#unless(defined($inputAbsPath)) { $inputAbsPath = ""; }
-#unless(defined($outputAbsPath)) { $outputAbsPath = ""; }
-#unless(defined($testRelPath)) { $testRelPath = ""; }
-#
-##while($ARGV[1] =~ m"(.*\\)*(.+)"g) {
-##  $filenames[0] = $2;
-##}
-#
-#print "===> CodeCruncher Copyright 2007 Eben Geer <===============================\n";
-#print "===> Input path: '$inputAbsPath' <===\n";
-#print "===> Output path: '$outputAbsPath' <===\n";
-##if(defined($ARGV[3])) {
-##  print "===> Test module path: '$testRelPath' <===\n";
-##}
-#
-#$filenames[0] = $indexRelPath;
-#print "\n===> Original File: $inputAbsPath$filenames[0] - Start <===\n";
-#while(<INPUT>) { $filestrings[0] .= $_; }
-#print $filestrings[0];
-#print "\n===> Original File: $inputAbsPath$filenames[0] - End <===\n";
-#
-## search for external files only goes one level deep
-#
-#print "\n===> External source files (.js): Identifying... <=========================\n";
-#while($filestrings[0] =~ m/src="(.*\.js)"/g) {
-#  $filenames[@filenames] = $1;
-#  print "$inputAbsPath$filenames[@filenames - 1]\n";
-#}
-#print "\n===> External source files (.js): Identified! <============================\n";
-#print "\n===> External source files (.css): Identifying... <========================\n";
-#while($filestrings[0] =~ m/url\((.*\.css)\)/g) {
-#  $filenames[@filenames] = $1;
-#  print "$inputAbsPath$filenames[@filenames - 1]\n";
-#}
-#print "\n===> External source files (.css): Identified! <===========================\n";
-#print "\n===> External source files: Opening... <===================================\n";
-#for(my $i = 1; $i < @filenames; $i++) {
-#  my $fh = new FileHandle("< $inputAbsPath$filenames[$i]");
-#  $filestrings[$i] = "";
-#  while(<$fh>) { $filestrings[$i] .= $_; }
-#  print "$inputAbsPath$filenames[$i]\n";
-#}
-#print "\n===> (" . scalar @filestrings - 1 . ") External source files: Opened! <==================================\n";
-#
-#for(my $i = 1; $i < @filenames; $i++) {
-#  print "\n===> External Source File: $inputAbsPath$filenames[$i] - Start <===\n";
-#  print $filestrings[$i];
-#  print "\n===> External Source File: $inputAbsPath$filenames[$i] - End <===\n";
-#}
-#
-#print "\n===> Comments: Extracting... <=============================================\n";
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  print "[$filenames[$i]]\n";
-#
-#  # HTML-style comments <!-- -->
-#  while($filestrings[$i] =~ s/(<!--(?!.{1,10}import).*?-->)//s) { print "$1\n"; }
-#
-#  # C-style block comments /* */
-#  while($filestrings[$i] =~ s"(/\*.*?\*/)""s) { print "$1\n"; }
-#
-#  # C++ style single-line comments // - and then ;//
-#  while($filestrings[$i] =~ s"(^//.*)"") { print "$1\n"; }
-#  while($filestrings[$i] =~ s"(\s//.*)"") { print "$1\n"; }
-#  while($filestrings[$i] =~ s";(//.*)";") { print "$1\n"; }
-#}
-#print "\n===> Comments: Extracted! <================================================\n";
-#print "\n===> Functions, Variables, and ID's: Identifying... <======================\n";
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  print "[$filenames[$i]]\n";
-#  while($filestrings[$i] =~ m/var\s+(\w+)/g) {
-#    #if($1 ne "rows") { $names[@names] = $1; }
-#    #$names[@names] = $1;
-#    $names[@names] = $1 unless(isAvoid($1));
-#    print "Identified Variable: '$names[@names - 1]'\n";
-#  }
-#}
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  print "[$filenames[$i]]\n";
-#  while($filestrings[$i] =~ m/function\s+(\w+)\s*\(/g) {
-#    $names[@names] = $1 unless(isAvoid($1));
-#    print "Identified Function: '$names[@names - 1]'\n";
-#  }
-#}
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  print "[$filenames[$i]]\n";
-#  while($filestrings[$i] =~ m/id="(\w+)"/g) {
-#    $names[@names] = $1 unless(isAvoid($1));
-#    print "Identified ID: '$names[@names - 1]'\n";
-#  }
-#}
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  print "[$filenames[$i]]\n";
-#  while($filestrings[$i] =~ m/(?<!meta )name="(\w+)"/g) {
-#    $names[@names] = $1 unless(isAvoid($1));
-#    print "Identified ID: '$names[@names - 1]'\n";
-#  }
-#}
-#print "\n===> Functions, Variables, and ID's: Identified! <=========================\n";
-#print "\n===> Functions, Variables, and ID's: Renaming... <=========================\n";
-#my @abbr;
-#my $n = @names;
-#my $offset = 0;
-#for(my $k = 0; $k < $n; $k++) {
-#  while(isKeyword($abbr[$k] = alphabase($k + $offset))) {
-#    $offset++;
-#  }
-#}
-#for(my $j = 0; $j < @names; $j++) {
-#  for(my $i = 0; $i < @filestrings; $i++) {
-#    #$filestrings[$i] =~ s/(?<![^\s\-\+\*\/=])($names[$j])(?![^\s\-\+\*\/=])/$abbr[$j]/g;
-#    #$filestrings[$i] =~ s/(?<=[\s\-\+\*\/\=\;\(\[\.\,])($names[$j])(?=[\s\-\+\*\/\=\;\(\)\]\.\,])/$abbr[$j]/g;
-#    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?![\w>])/$abbr[$j]/g;
-#    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?! ?[\w])/$abbr[$j]/g;
-#    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?! ?[\w])(?![>])/$abbr[$j]/g;
-#    #$filestrings[$i] =~ s/(?<![\w'<])($names[$j])(?! ?[\w'])(?![>])/$abbr[$j]/g;
-#    $filestrings[$i] =~ s/(?<![\w<])(?<!== ')($names[$j])(?! ?[\w])(?![>])/$abbr[$j]/g;
-#  }
-#  print "'$abbr[$j]' substituted for '$names[$j]'\n";
-#}
-#print "\n===> Functions, Variables, and ID's: Renamed! <============================\n";
-#
-#for(my $i = 0; $i < @filenames; $i++) {
-#  print "\n===> Source File Before Whitespace Removal: $inputAbsPath$filenames[$i] - Start <===\n";
-#  print $filestrings[$i];
-#  print "\n===> Source File Before Whitespace Removal: $inputAbsPath$filenames[$i] - End <===\n";
-#}
-#
-#if(0) {
-#print "\n===> Whitespace: Extracting... <===========================================\n";
-#for(my $i = 0; $i < @filestrings; $i++) {
-#  # = + -
-#  $filestrings[$i] =~ s/(?<!')\s*=\s*(?!')/=/g;
-#  $filestrings[$i] =~ s/(?<!')\s*\+\s*(?!')/\+/g;
-#  $filestrings[$i] =~ s/(?<!')\s*-\s*(?!')/-/g;
-#
-#  # < > || &&
-#  $filestrings[$i] =~ s/\s*<\s*/</g;
-#  $filestrings[$i] =~ s/\s*>\s*/>/g;
-#  $filestrings[$i] =~ s/\s*\|\|\s*/\|\|/g;
-#  $filestrings[$i] =~ s/\s*\&\&\s*/\&\&/g;
-#
-#  # ( ) { }
-#  $filestrings[$i] =~ s/\s*\(\s*/\(/g;
-#  $filestrings[$i] =~ s/\s*\)\s*/\)/g;
-#  $filestrings[$i] =~ s/\s*\{\s*/\{/g;
-#  $filestrings[$i] =~ s/\s*\}\s*/\}/g;
-#
-#  # leading and trailing whitespace
-#  $filestrings[$i] =~ s/;\s+/;/g;
-#  $filestrings[$i] =~ s/\n\s*//g;
-#
-#  print "[$filenames[$i]]\n";
-#  print "$filestrings[$i]\n";
-#}
-#}
-#print "\n===> Whitespace: Extracted! <==============================================\n";
-#print "\n===> Output files: Writing... <============================================\n";
-#for(my $i = 0; $i < @filenames; $i++) {
-#  print "$outputAbsPath$filenames[$i]\n";
-#  open EXTERNAL, "> $outputAbsPath$filenames[$i]" or die "$outputAbsPath$filenames[$i] - $!\n";
-#  print EXTERNAL $filestrings[$i];
-#  close EXTERNAL;
-#}
-#print "\n===> Output files: Written! <==============================================\n";
-#
-##if(defined($ARGV[3])) {
-#  print "\n===> Test Module: Updating... <============================================\n";
-#  open TESTIN, "< $inputAbsPath$testRelPath" or die "$inputAbsPath$testRelPath - $!\n";
-#  while(<TESTIN>) { $teststring .= $_; }
-#  close TESTIN;
-#  for(my $j = 0; $j < @names; $j++) {
-#    $teststring =~ s/(?<![\w"<])($names[$j])(?! ?[\w"])(?![>])/$abbr[$j]/g;
-#    print "'$abbr[$j]' substituted for '$names[$j]'\n";
-#  }
-#  open TESTOUT, "> $outputAbsPath$testRelPath" or die "$outputAbsPath$testRelPath - $!\n";
-#  print TESTOUT $teststring;
-#  close TESTOUT;
-#  print "\n===> Test Module: Updated! <===============================================\n";
-##}
-#
-#print "\n===> CodeCruncher Finished! <==============================================\n";
-#
+$inputRelPath = $inputRelPath;
+
+
+# search for external files only goes one level deep
+
+print "\n===> External source files (.js): Identifying... <=========================\n";
+while($filestrings[0] =~ m/src="(.*\.js)"/g) {
+  $filenames[@filenames] = $1;
+  print "$inputRelPath$filenames[@filenames - 1]\n";
+}
+print "\n===> External source files (.js): Identified! <============================\n";
+print "\n===> External source files (.css): Identifying... <========================\n";
+while($filestrings[0] =~ m/url\((.*\.css)\)/g) {
+  $filenames[@filenames] = $1;
+  print "$inputRelPath$filenames[@filenames - 1]\n";
+}
+print "\n===> External source files (.css): Identified! <===========================\n";
+print "\n===> External source files: Opening... <===================================\n";
+for(my $i = 1; $i < @filenames; $i++) {
+  my $fh = new FileHandle("< $inputRelPath$filenames[$i]");
+  $filestrings[$i] = "";
+  while(<$fh>) { $filestrings[$i] .= $_; }
+  print "$inputRelPath$filenames[$i]\n";
+}
+print "\n===> (" . scalar @filestrings - 1 . ") External source files: Opened! <==================================\n";
+
+for(my $i = 1; $i < @filenames; $i++) {
+  print "\n===> External Source File: $inputRelPath$filenames[$i] - Start <===\n";
+  print $filestrings[$i];
+  print "\n===> External Source File: $inputRelPath$filenames[$i] - End <===\n";
+}
+
+print "\n===> Comments: Extracting... <=============================================\n";
+for(my $i = 0; $i < @filestrings; $i++) {
+  print "[$filenames[$i]]\n";
+
+  # HTML-style comments <!-- -->
+  while($filestrings[$i] =~ s/(<!--(?!.{1,10}import).*?-->)//s) { print "$1\n"; }
+
+  # C-style block comments /* */
+  while($filestrings[$i] =~ s"(/\*.*?\*/)""s) { print "$1\n"; }
+
+  # C++ style single-line comments // - and then ;//
+  while($filestrings[$i] =~ s"(^//.*)"") { print "$1\n"; }
+  while($filestrings[$i] =~ s"(\s//.*)"") { print "$1\n"; }
+  while($filestrings[$i] =~ s";(//.*)";") { print "$1\n"; }
+}
+print "\n===> Comments: Extracted! <================================================\n";
+print "\n===> Functions, Variables, and ID's: Identifying... <======================\n";
+for(my $i = 0; $i < @filestrings; $i++) {
+  print "[$filenames[$i]]\n";
+  while($filestrings[$i] =~ m/var\s+(\w+)/g) {
+    #if($1 ne "rows") { $names[@names] = $1; }
+    #$names[@names] = $1;
+    $names[@names] = $1 unless(isAvoid($1));
+    print "Identified Variable: '$names[@names - 1]'\n";
+  }
+}
+for(my $i = 0; $i < @filestrings; $i++) {
+  print "[$filenames[$i]]\n";
+  while($filestrings[$i] =~ m/function\s+(\w+)\s*\(/g) {
+    $names[@names] = $1 unless(isAvoid($1));
+    print "Identified Function: '$names[@names - 1]'\n";
+  }
+}
+for(my $i = 0; $i < @filestrings; $i++) {
+  print "[$filenames[$i]]\n";
+  while($filestrings[$i] =~ m/id="(\w+)"/g) {
+    $names[@names] = $1 unless(isAvoid($1));
+    print "Identified ID: '$names[@names - 1]'\n";
+  }
+}
+for(my $i = 0; $i < @filestrings; $i++) {
+  print "[$filenames[$i]]\n";
+  while($filestrings[$i] =~ m/(?<!meta )name="(\w+)"/g) {
+    $names[@names] = $1 unless(isAvoid($1));
+    print "Identified ID: '$names[@names - 1]'\n";
+  }
+}
+print "\n===> Functions, Variables, and ID's: Identified! <=========================\n";
+print "\n===> Functions, Variables, and ID's: Renaming... <=========================\n";
+my @abbr;
+my $n = @names;
+my $offset = 0;
+for(my $k = 0; $k < $n; $k++) {
+  while(isKeyword($abbr[$k] = alphabase($k + $offset))) {
+    $offset++;
+  }
+}
+for(my $j = 0; $j < @names; $j++) {
+  for(my $i = 0; $i < @filestrings; $i++) {
+    #$filestrings[$i] =~ s/(?<![^\s\-\+\*\/=])($names[$j])(?![^\s\-\+\*\/=])/$abbr[$j]/g;
+    #$filestrings[$i] =~ s/(?<=[\s\-\+\*\/\=\;\(\[\.\,])($names[$j])(?=[\s\-\+\*\/\=\;\(\)\]\.\,])/$abbr[$j]/g;
+    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?![\w>])/$abbr[$j]/g;
+    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?! ?[\w])/$abbr[$j]/g;
+    #$filestrings[$i] =~ s/(?<![\w<\.])($names[$j])(?! ?[\w])(?![>])/$abbr[$j]/g;
+    #$filestrings[$i] =~ s/(?<![\w'<])($names[$j])(?! ?[\w'])(?![>])/$abbr[$j]/g;
+    $filestrings[$i] =~ s/(?<![\w<])(?<!== ')($names[$j])(?! ?[\w])(?![>])/$abbr[$j]/g;
+  }
+  print "'$abbr[$j]' substituted for '$names[$j]'\n";
+}
+print "\n===> Functions, Variables, and ID's: Renamed! <============================\n";
+
+for(my $i = 0; $i < @filenames; $i++) {
+  print "\n===> Source File Before Whitespace Removal: $inputRelPath$filenames[$i] - Start <===\n";
+  print $filestrings[$i];
+  print "\n===> Source File Before Whitespace Removal: $inputRelPath$filenames[$i] - End <===\n";
+}
+
+if(0) {
+print "\n===> Whitespace: Extracting... <===========================================\n";
+for(my $i = 0; $i < @filestrings; $i++) {
+  # = + -
+  $filestrings[$i] =~ s/(?<!')\s*=\s*(?!')/=/g;
+  $filestrings[$i] =~ s/(?<!')\s*\+\s*(?!')/\+/g;
+  $filestrings[$i] =~ s/(?<!')\s*-\s*(?!')/-/g;
+
+  # < > || &&
+  $filestrings[$i] =~ s/\s*<\s*/</g;
+  $filestrings[$i] =~ s/\s*>\s*/>/g;
+  $filestrings[$i] =~ s/\s*\|\|\s*/\|\|/g;
+  $filestrings[$i] =~ s/\s*\&\&\s*/\&\&/g;
+
+  # ( ) { }
+  $filestrings[$i] =~ s/\s*\(\s*/\(/g;
+  $filestrings[$i] =~ s/\s*\)\s*/\)/g;
+  $filestrings[$i] =~ s/\s*\{\s*/\{/g;
+  $filestrings[$i] =~ s/\s*\}\s*/\}/g;
+
+  # leading and trailing whitespace
+  $filestrings[$i] =~ s/;\s+/;/g;
+  $filestrings[$i] =~ s/\n\s*//g;
+
+  print "[$filenames[$i]]\n";
+  print "$filestrings[$i]\n";
+}
+}
+print "\n===> Whitespace: Extracted! <==============================================\n";
+print "\n===> Output files: Writing... <============================================\n";
+for(my $i = 0; $i < @filenames; $i++) {
+  print "$outputRelPath$filenames[$i]\n";
+  open EXTERNAL, "> $outputRelPath$filenames[$i]" or die "$outputRelPath$filenames[$i] - $!\n";
+  print EXTERNAL $filestrings[$i];
+  close EXTERNAL;
+}
+print "\n===> Output files: Written! <==============================================\n";
+
+#if(defined($ARGV[3])) {
+if(defined($updatePaths[0])) {
+  print "\n===> Test Module: Updating... <============================================\n";
+  open TESTIN, "< $inputRelPath$updatePaths[0]" or die "$inputRelPath$updatePaths[0] - $!\n";
+  while(<TESTIN>) { $teststring .= $_; }
+  close TESTIN;
+  for(my $j = 0; $j < @names; $j++) {
+    $teststring =~ s/(?<![\w"<])($names[$j])(?! ?[\w"])(?![>])/$abbr[$j]/g;
+    print "'$abbr[$j]' substituted for '$names[$j]'\n";
+  }
+  open TESTOUT, "> $outputRelPath$updatePaths[0]" or die "$outputRelPath$updatePaths[0] - $!\n";
+  print TESTOUT $teststring;
+  close TESTOUT;
+  print "\n===> Test Module: Updated! <===============================================\n";
+}
+
+print "\n===> CodeCruncher Finished! <==============================================\n";
+
 #close INPUT;
-##close OUTPUT;
+#close OUTPUT;
 #close LOG;
-#
-#
-#
+
+
+
